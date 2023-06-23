ca5ac8299e74b7f4b06db7e550d687f2290550fa
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2023-06-24 00:31:28 +0200

    New cookbook source file

---
+++ src/signal_handler.c
@@ -0,0 +1,21 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void handler_interrupt(int val) { 
+    printf(" -> %d received\n", val); 
+    exit(0);
+}
+
+void handler_quit(int val) { 
+    printf(" -> %d received\n", val); 
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    signal(SIGINT, handler_interrupt);
+    signal(SIGQUIT, handler_quit);
+    while (1)
+        ;
+    exit(0);
+}
